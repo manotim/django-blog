c411fac7d5443b005acf435f6d3d38048a63454b
manotim <manotimike@gmail.com>
2023-10-16 09:36:52 +0300

    CRUD, Create ongoing

--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,24 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'myblog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = "myblog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 
 def about(request):
     return render(request, 'myblog/about.html', {'title': 'About'})
